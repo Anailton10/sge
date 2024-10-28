bc97be62fd7771a0be91faa39695f4c718d59b80
Anailton <francisco.anailton.17@gmail.com>
2024-10-28 14:09:27 -0300

    feat:implementing butom search for filter brands

--- brands/views.py
+++ brands/views.py
@@ -8,3 +8,12 @@ class BrandListaView(generic.ListView):
     model = Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+
+    def get_queryset(self):
+        queryste = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryste = queryste.filter(name__icontains=name)
+
+        return queryste
