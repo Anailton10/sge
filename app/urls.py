f2be38da385a435efbb057c8173239696989ffed
Anailton <francisco.anailton.17@gmail.com>
2024-11-13 10:15:58 -0300

    feat: login

--- app/urls.py
+++ app/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.home, name='home'),
     path('brands/', include('brands.urls')),
     path('categories/', include('categories.urls')),
